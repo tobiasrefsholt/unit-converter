92e6c176c00b65c3e4f0d8a19ef92b921f947129
Tobias Refsholt <tobias@refsholt.net>
2023-09-26 14:17:05 +0200

    Added calculate functions

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,15 @@
 "use strict";
+
+function getInput(input) {
+    const inputUnit = model.app.selectedUnit.input;
+    const unitFactor = model.units[inputUnit].factor;
+    model.app.inputInMeters = input / unitFactor;
+}
+
+function calculateOutput() {
+    const outputUnit = model.app.selectedUnit.output;
+    const outputFactor = model.units[outputUnit].factor;
+    const inputInMeters = model.app.inputInMeters;
+    model.fields.output = outputFactor * inputInMeters;
+    updateView();
+}
