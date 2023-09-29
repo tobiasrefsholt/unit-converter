3bc8dfe132d2b065d1367bde3af930f7f12c8f2a
Tobias Refsholt <tobias@refsholt.net>
2023-09-29 23:25:43 +0200

    Volume units

--- js/model.js
+++ js/model.js
@@ -105,7 +105,84 @@ const model = {
                 },
             }
         },
-        volume: {},
+        volume: {
+            selectedUnit: {
+                input: "liter",
+                output: "usCup",
+            },
+            units: {
+                cubucMeter: {
+                    factor: 1 * 10**3,
+                },
+                cubucKilometer: {
+                    factor: 1 * 10**12,
+                },
+                cubucCenitmeter: {
+                    factor: 1 * 10**(-3),
+                },
+                cubucMillieter: {
+                    factor: 1 * 10**(-6),
+                },
+                liter: {
+                    factor: 1,
+                },
+                milliliter: {
+                    factor: 1 * 10**(-3),
+                },
+                usGallon: {
+                    factor: 3.78541,
+                },
+                usQuart: {
+                    factor: 0.9463525,
+                },
+                usPint: {
+                    factor: 0.47317625,
+                },
+                usCup: {
+                    factor: 0.236588125,
+                },
+                usFluidOunce: {
+                    factor: 0.0295735156,
+                },
+                usTableSpoon: {
+                    factor: 0.0147867578,
+                },
+                usTeaSpoon: {
+                    factor: 0.0049289193,
+                },
+                imperialGallon: {
+                    factor: 4.54609,
+                },
+                imperialQuart: {
+                    factor: 1.1365225,
+                },
+                imperialPint: {
+                    factor: 0.56826125,
+                },
+                imperialFluidOunce: {
+                    factor: 0.0284130625,
+                },
+                imperiaTableSpoon: {
+                    factor: 0.0177581641,
+                },
+                imperialTeaSpoon: {
+                    factor: 0.005919388,
+                },
+                cubicMile: {
+                    factor: 4168180000000,
+                },
+                cubicYard: {
+                    factor: 764.55485798,
+                },
+                cubicFoot: {
+                    factor: 28.316846592,
+                },
+                cubicInch: {
+                    factor: 0.016387064,
+                },
+            }
+
+        },
         weight: {},
         time: {}
     }
